1fd6399ba598bccc5ea024cd2c70e3654a181001
rommzz <rommy_zohara@yahoo.comgit config --global user.email rommy_zohara@yahoo.com>
2021-03-11 23:06:38 +0700

    base app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          src="https://glovory-mart.netlify.app/icons/logo-color.svg"
+          contain
+          height="50"
+        />
+        <v-autocomplete
+          clearable
+          dense
+          solo
+          flat
+          hide-details=true
+          label="search product..."
+        ></v-autocomplete>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2 black--text">Latest Release</span>
+        <v-icon color="black">mdi-plus</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Home/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './views/Home';
+import axios  from "axios";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Home,
+  },
+  data (){
+    return{
+      item: []
+    }
+  },
+  created () {
+    this.getItem()
+  },
+  mounted () {
+    
+  },
+  methods: {
+    getItem(){
+      axios.get('https://glovorymart.glitch.me/api/products')
+      .then((response) => {
+        // handle success
+        this.item = response
+        console.log(this.item);
+        
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+    }
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-container>
+    <v-card
+      flat
+      class="py-12"
+    >
+      <v-card-text>
+        <v-row
+          align="center"
+          justify="center">
+          <v-btn-toggle
+            color="red darken-1">
+            <v-btn v-for="(item, index) in summary" :key="index">
+              <div white--text>
+                {{item}}
+              </div>
+            </v-btn>
+          </v-btn-toggle>
+        </v-row>
+      </v-card-text>
+    </v-card>
+    <v-row class="text-center">
+      <v-col cols="6" sm="4" md="4" lg="3" xl="2"  v-for="(data, index) in item" :key="index">
+        <v-card
+          class="mx-auto"
+          max-width="344">
+          <v-list-item-avatar
+            tile
+            size="100"
+            color="grey">
+            <v-img
+              :aspect-ratio="1/1"
+              width="100"
+              :src="data.image_url"
+            ></v-img>
+            </v-list-item-avatar>
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="overline mb-4" style="border: blue">
+                {{ data.category }}
+              </div>
+              <div v-if="data.is_free" class="overline mb-4" style="border: blue">
+                {{'Free ' + data.max_purchase + 'item/day' }}
+              </div>
+              <v-list-item-title class="headline mb-1">
+                {{ data.name }}
+              </v-list-item-title>
+              <v-list-item-subtitle>{{'Rp. ' + data.base_price }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-card-actions>
+            <v-btn
+              outlined
+              rounded
+              text
+            >
+              Button
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: 'Home',
+
+  data (){
+    return{
+      item: [],
+      summary: []
+    }
+  },
+  created () {
+    this.getItem()
+  },
+  mounted () {
+    
+  },
+  methods: {
+    getItem(){
+      axios.get('https://glovorymart.glitch.me/api/products')
+      .then((response) => {
+        // handle success
+        this.summary = response.data.summary
+        this.item = response.data.data
+        console.log(this.summary);
+        
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+    }
+  }
+}
+</script>
